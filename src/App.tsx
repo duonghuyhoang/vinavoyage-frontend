ce0b02678b6dfdd1dc1f88961ec79c9761087ff3
HoangDH <hoangdh@omegatheme.com>
2025-10-03 16:35:50 +0700

    chore: initial commit with React 18 + Webpack setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactlogo from './assets/images/react.svg'
-import './assets/styles/app.css'
 import './assets/styles/app.scss'
 
 const App: FC = () => {
@@ -10,6 +9,7 @@ const App: FC = () => {
     <div>
       <img src={reactlogo} alt='React Logo' width={100} height={100} />
       <h1>{fullname}</h1>
+      123
     </div>
   )
 }
